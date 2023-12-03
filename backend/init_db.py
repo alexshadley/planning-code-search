ff944cfb71dded10a505fc56fbc521ddee396bd3
lowrollr <92640744+lowrollr@users.noreply.github.com>
2023-12-02 20:28:58 -0800

    fix init script

--- backend/init_db.py
+++ backend/init_db.py
@@ -4,10 +4,13 @@ import os
 
 from backend.load import init_db_from_documents
 
+import dotenv
+
+dotenv.load_dotenv()
+
 if __name__ == '__main__':
         
     embeddings_model = OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY"))
-    embeddings_fn = embeddings_model.embed
 
     documents = ["san_francisco-ca-1.txt"]
     init_db_from_documents(documents, embeddings_model)
